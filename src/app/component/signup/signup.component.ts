0f35d50862914b6a17b7bcb929f4b54bff8b5614
Santiago Leza <santiago.leza@hotmail.com>
2023-05-12 15:29:30 -0300

    0.15

--- src/app/component/signup/signup.component.ts
+++ src/app/component/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +8,47 @@ import { Component } from '@angular/core';
 })
 export class SignupComponent {
 
+  form: FormGroup;
+  pass = {first: "", last: ""};
+  passValid = false;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      confirmPassword: ['', [Validators.required]],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      country: ['', ],
+      city: ['', ]
+    });
+
+    this.form.addValidators(this.checkPassword);
+  }
+
   onSubmit(event: Event){
     
-    event.preventDefault();
-    
+    if(this.form.valid){
+      
+    }
+    else{
+      this.form.markAllAsTouched();
+    }
+  }
+
+  checkPassword: ValidatorFn = (group: AbstractControl): ValidationErrors | null => {
+    const password = this.form.get('password')?.value;
+    const confirmPassword = this.form.get('confirmPassword')?.value;
+
+    console.log(password);
+    console.log(confirmPassword);
+    if (password === confirmPassword) {
+      this.passValid = true;
+    }
+    else{
+      this.passValid = false;
+    }
+
+    return password.valueOf === confirmPassword.valueOf ? null : { notSame: true };
   }
 }
